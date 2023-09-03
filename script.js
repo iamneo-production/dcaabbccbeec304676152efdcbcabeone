53f5c99e3e446a2e2996e02eea332ab846e6bef0
Iamneo <techadmin@iamneo.ai>
2023-09-03 13:43:32 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ let currentPlayer='X';
 let isGameActive=true;
 function handleCellClick(event){
     const cell=event.target;
-    if (cell)
+    if (cell.textContent==='' && isGameActive){
+        cell.textContent=currentPlayer;
+        checkWinner();
+        togglePlayer();
+    }
+}
+function checkWinner(){
+    const winningCombos=[
+        [0,1,2],[3,4,5],[6,7,8],
+    ]
 }
