420339a2a09b7d5c641f92188dc6691f35e6bfdd
Iamneo <techadmin@iamneo.ai>
2023-09-03 14:23:32 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,64 +1,47 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Your game logic here
-
-    /*
-    **Part 1: Winning Conditions (Add your code here)**
-
-    1. Implement the logic to check for winning conditions using the 'conditions' array.
-    2. Display a winning message in the 'result' element when a player wins.
-    3. Disable all buttons after a win.
-    */
-
-    // Your code to update the game state and check for a win
-    // ...
-
-    // Your code to display the current player's turn
-    // ...
-
-    // Your code to handle button and cell interactions
-    // ...
-};
-
-    /*
-    **Part 2: Reset Function (Add your code here)**
-
-    1. Implement a new function that resets the game to its initial state.
-    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-    3. Update the 'result' element to indicate the current player's turn.
-    4. Re-enable all buttons for a new game.
-    */
-
-// Function to reset the game
-const resetGame = () => {
-    // Your code to reset the game state
-    // ...
-
-    // Your code to update the 'result' element
-    // ...
-
-    // Your code to re-enable buttons
-    // ...
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
+const cells=document.querySelectorAll('.btn');
+const resultText=document.querySelector('.result');
+const resetButton=document.getElementById('reset-button');
+let currentPlayer='X';
+let isGameActive=true;
+function handleCellClick(event){
+    const cell=event.target;
+    if (cell.textContent==='' && isGameActive){
+        cell.textContent=currentPlayer;
+        checkWinner();
+        togglePlayer();
+    }
+}
+function checkWinner(){
+    const winningCombos=[
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ];
+    for(const combo of winningCombos){
+        const [a,b,c]=combo;
+        if(cells[a].textContent&&cells[a].textContent===cells[b].textContent && cells[a].textContent===cells[c].textContent){
+            resultText.textContent='Player ${currentPlayer} wins!';
+            isGameActive=false;
+            resetButton.disabled=false;
+        }
+    }
+    if ([...cells].every(cell=>cell.textContent !=='')){
+        resultText.textContent="It's a draw!";
+        isGameActive=false;
+        resetButton.disabled=false;
+    }
+}
+function handleReset(){
+    cells.forEach(cell=>{
+        cell.textContent='';
+    });
+    currentPlayer='X';
+    resultText.textContent="player X's turn";
+    isGameActive=true;
+    resetButton.disabled=true;
+}
+cells.forEach(cell=>{
+    cell.addEventListener('click',handleCellClick);
 });
-
-document.querySelector('#reset').addEventListener('click', resetGame);
+resetButton.addEventListener('click',handleReset);
+resultText.textContent="player X's turn";
